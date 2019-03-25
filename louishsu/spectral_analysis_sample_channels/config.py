0330d8eb4f78cb095d41d3534b0c9dd58077b9ff
louishsu <is.louishsu@foxmail.com>
2019-03-25 20:44:34 +0800

    20190325

--- louishsu/spectral_analysis_sample_channels/config.py
+++ louishsu/spectral_analysis_sample_channels/config.py
@@ -6,7 +6,7 @@ configer.datapath = "/datasets/ECUST2019"
 configer.logspath = "/home/louishsu/Work/Workspace/HUAWEI/pytorch/logs"
 configer.mdlspath = "/home/louishsu/Work/Workspace/HUAWEI/pytorch/modelfiles"
 
-configer.splitmode  = 'split_2'
+configer.splitmode  = 'split_5'
 
 configer.facesize   = (64, 64)
 configer.n_channels = 1
@@ -14,14 +14,13 @@ configer.n_classes  = 40
 
 # configer.modelname = "analysis_vgg11_channels"
 # configer.modelname = "{}_{}".format(configer.modelname, configer.splitmode)
-# configer.modelname = "analysis_vgg11_channels_split1"
-configer.modelname = "analysis_vgg11_channels_split2"
+configer.modelname = "analysis_vgg11_channels_split5"
 
 configer.lossname  = "crossent"
 configer.learningrate = 0.001
 configer.batchsize = 256
-configer.n_epoch   = 300
+configer.n_epoch   = 30
 configer.earlystopping = True
 
-configer.stepsize = 120
+configer.stepsize = 20
 configer.gamma = 0.2
