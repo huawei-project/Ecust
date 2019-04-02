a7fd5c34a942f2f7d30da8b6b3efeced2ddf1f0b
louishsu <is.louishsu@foxmail.com>
2019-04-02 11:41:13 +0800

    split done

---
+++ louishsu/rewrite_recognize/datasets.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+import numpy as np
+
+from torch.utils.data       import Dataset
+from torchvision.transforms import ToTensor
+
+class RecognizeDataset(Dataset):
+    """ 识别数据集
+    """
+
+    def __init__(self, type, mode):
+        """
+        Params:
+            type:   {str} 'Multi', 'RGB'
+            mode:   {str} 'train', 'valid', 'test'
+        """
+
+        
