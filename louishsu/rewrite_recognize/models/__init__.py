74b28dcc85c069eee595779a66a2796c7a848a4c
louishsu <is.louishsu@foxmail.com>
2019-04-10 16:32:46 +0800

    lstm

--- louishsu/rewrite_recognize/models/__init__.py
+++ louishsu/rewrite_recognize/models/__init__.py
@@ -4,14 +4,12 @@ from .shufflenet_v2 import ShuffleNetV2
 from .squeezenet import SqueezeNet
 from .mobilenet_v2 import MobileNetV2
 from .densenet import DenseNet
-from lstm import ConvLSTM
+from .lstm import ConvLSTM
 
 modeldict = {
     'recognize_vgg11_bn': lambda inp, outp, size: VGG(inp, outp, size, 'VGG11', batch_norm=True),
     'recognize_shufflev2': lambda inp, outp, size: ShuffleNetV2(inp, outp, size),
     'recognize_squeeze_v11': lambda inp, outp, size: SqueezeNet(inp, outp, version=1.1),
     'recognize_dense121': lambda inp, outp, size: DenseNet(in_channels=inp, num_classes=outp),
-    'recognize_convlstm': lambda inp, outp, size: ConvLSTM(1, outp, size, inp, 
-                                                        base_model=lambda inp, outp: VGG(inp, outp, size, 'VGG11', 
-                                                                                        init_weights=True, finetune=False)),
+    'recognize_convlstm': lambda inp, outp, size: ConvLSTM(1, outp, size, inp),
 }
