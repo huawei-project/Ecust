8621e6b2adb1de499f57da5e0f7f40565a8deb7c
louishsu <is.louishsu@foxmail.com>
2019-04-16 10:33:04 +0800

    gen excel

--- louishsu/rewrite_recognize/main.py
+++ louishsu/rewrite_recognize/main.py
@@ -1,10 +1,12 @@
 from train import train
 from test  import test
+from gen_excel import gen_out_excel
 from config import configer
 
 def main():
     train(configer)
     test(configer)
+    gen_out_excel(configer)
 
 if __name__ == "__main__":
     main()
