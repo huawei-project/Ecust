9551f04bd8cedfb655a77528341899402254b19b
louishsu <is.louishsu@foxmail.com>
2019-04-17 17:40:43 +0800

    best channels

--- louishsu/rewrite_recognize/main_update_config.py
+++ louishsu/rewrite_recognize/main_update_config.py
@@ -69,7 +69,7 @@ def main_best_channels():
             if datatype == 'Multi':
                 usedChannelsList = [[i] for i in range(23)]
             else:
-                usedChannelsList = ['R', 'G', 'B']:
+                usedChannelsList = ['R', 'G', 'B']
 
             for usedChannels in usedChannelsList:
                 
@@ -122,18 +122,18 @@ def main_best_channels():
 def main_several_channels():
 
     # 波段选择依据
-    # 以最佳的划分方式: 
+    # 以最佳的划分方式: 0.6: 0.2: 0.2(5个)
     # 最优的波段排序: 
+    #       [850, 870, 930, 730, 790, 910, 770, 750, 670, 950, 990, 830, 890, 810, 970, 690, 710, 650, 590, 570, 630, 610, 550]
     # 依次选择多个波段进行实验
+    CHANNEL_SORT = [850, 870, 930, 730, 790, 910, 770, 750, 670, 950, 990, 830, 890, 810, 970, 690, 710, 650, 590, 570, 630, 610, 550]
+    
+    for splitidx in range(1, 6):
+        usedChannelsList = [CHANNEL_SORT[:i+2] for i in range(23)]
 
-    for splitidx in range(6, 36):
-        usedChannelsList = [
-            [750, 850, 950],    # TODO
-
-        ]
         for usedChannels in usedChannelsList:
-             
-            print(getTime(), splitidx, usedChannels, '...')
+            
+            print(getTime(), splitidx, len(usedChannels), '...')
 
             configer = EasyDict()
 
