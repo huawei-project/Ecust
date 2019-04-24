395b78e620689af3c4a6e8432ea8e9861ce18b33
louishsu <is.louishsu@foxmail.com>
2019-04-24 14:25:40 +0800

    inception

--- louishsu/rewrite_recognize/models/__init__.py
+++ louishsu/rewrite_recognize/models/__init__.py
@@ -6,6 +6,7 @@ from .mobilenet_v2 import MobileNetV2
 from .densenet import DenseNet
 from .lstm import ConvLSTM, ConvLSTMNet
 from .resnet34 import ResNet
+from .inceptionv3 import InceptionV3
 
 modeldict = {
     'recognize_vgg11_bn': lambda inp, outp, size: VGG(inp, outp, size, 'VGG11', batch_norm=True),
@@ -14,5 +15,6 @@ modeldict = {
     'recognize_dense121': lambda inp, outp, size: DenseNet(in_channels=inp, num_classes=outp),
     'recognize_convlstm': lambda inp, outp, size: ConvLSTM(1, outp, size, inp),
     'recognize_convlstm2': lambda inp, outp, size: ConvLSTMNet(1, outp, size, inp),
-    'recognize_resnet34': lambda inp, outp,size: ResNet(in_channels=inp,num_classes=outp),
+    'recognize_resnet34': lambda inp, outp, size: ResNet(in_channels=inp,num_classes=outp),
+    'recognize_inceptionv3': lambda inp, outp, size: InceptionV3(in_channels=inp, num_classes=outp),
 }
