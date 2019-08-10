abda94087a8f1243801d8cd33c513bcf6a8d1a6b
louishsu <is.louishsu@foxmail.com>
2019-08-10 11:17:23 +0800

    Time: 2019-08-10 11:17:22

--- louishsu/recognize_stage_2/main_update_config.py
+++ louishsu/recognize_stage_2/main_update_config.py
@@ -4,7 +4,7 @@
 @Auther: louishsu
 @E-mail: is.louishsu@foxmail.com
 @Date: 2019-08-10 10:30:40
-@LastEditTime: 2019-08-10 10:47:35
+@LastEditTime: 2019-08-10 11:16:55
 @Update: 
 '''
 import os
@@ -204,7 +204,7 @@ def main_3_2(make_table_figure=False):
             ## 做表格
             head_name = "count/波段索引"
             rows_name = [str(i) for i in splitcounts] + ['average']
-            cols_name = ["{:.2f}: {:.2f}: 0.2".format(i, 0.8 - i) for i in trains]
+            cols_name = [str(i+1) for i in range(W)] if datatype == "Multi" else ["R", "G", "B"]
             
             table_acc  = gen_markdown_table_2d(head_name, rows_name, cols_name, table_data_acc)
             table_loss = gen_markdown_table_2d(head_name, rows_name, cols_name, table_data_loss)
@@ -237,8 +237,10 @@ def main_3_2(make_table_figure=False):
 
     start_time = time.time(); elapsed_time = 0
 
-    for datatype in datatypes:
+    for i in range(len(datatypes)):
 
+        datatype = datatypes[i]
+        
         data_acc  = np.zeros(shape=(H, W))
         data_loss = np.zeros(shape=(H, W))
         
@@ -275,13 +277,167 @@ def main_3_2(make_table_figure=False):
 
         np.savetxt("images/3_2_<data>_[{}].txt".format(datatype), table_data)
 
-def main_3_3():
+def main_3_3(make_table_figure=False):
 
-    pass
+    ORDER = [i+1 for i in range(25)]  # TODO
+    usedChannels_list = [ORDER[: i+1] for i in range(len(ORDER))]
+    splitcounts = [i for i in range(1, 6)]
+    H, W = len(splitcounts), len(usedChannels_list)
+
+    if make_table_figure:
+
+        print("Generating tables and figures [Multi]...")
+
+        table_data = np.loadtxt("images/3_3_<data>_[Multi].txt")
+        table_data_acc, table_data_loss = np.vsplit(table_data, 2)      # 按竖直方向划分为两块，即上下
+
+        ## 做表格
+        head_name = "count/组合数"
+        rows_name = [str(i) for i in splitcounts] + ['average']
+        cols_name = [str(i+1) for i in range(W)]
+        
+        table_acc  = gen_markdown_table_2d(head_name, rows_name, cols_name, table_data_acc)
+        table_loss = gen_markdown_table_2d(head_name, rows_name, cols_name, table_data_loss)
+    
+        with open("images/3_3_<table>_[Multi].txt", 'w') as f:
+            f.write("\n\nacc\n")
+            f.write(table_acc)
+            f.write("\n\nloss\n")
+            f.write(table_loss)
+        
+        ## 作图
+        plt.figure()
+        
+        plt.subplot(121); plt.title("acc")
+        avg_acc = table_data_acc[-1]
+        plt.bar(np.arange(avg_acc.shape[0]),  avg_acc )
+        
+        plt.subplot(122); plt.title("loss")
+        avg_loss = table_data_loss[-1]
+        plt.bar(np.arange(avg_loss.shape[0]), avg_loss)
+
+        plt.savefig("images/3_3_<figure>_[Multi].png")
+
+        return
+
+    start_time = time.time(); elapsed_time = 0
+
+    data_acc  = np.zeros(shape=(H, W))
+    data_loss = np.zeros(shape=(H, W))
+    
+    for i in range(len(splitcounts)):
+        splitcount = splitcounts[i]
+        
+        for j in range(len(usedChannels_list)):
+            usedChannels = usedChannels_list[j]
+            
+            configer = get_configer(splitcount=splitcount, usedChannels=usedChannels)
+
+            elapsed_time += time.time() - start_time
+            start_time    = time.time()
+            print("Main 3.3 [Multi] [{}] {}... Elaped >>> {} min".\
+                        format(configer.splitmode, usedChannels, elapsed_time/60))
+
+            logpath = os.path.join(configer.logspath, configer.modelname)
+            if os.path.exists(logpath):
+                with open(os.path.join(logpath, 'test_log.txt'), 'r') as f:
+                    test_log = f.readlines()
+                    data_acc[i, j], data_loss[i, j] = parse_log(test_log)
+                    print(test_log)
+            else:
+                train(configer)
+                data_acc[i, j], data_loss[i, j] = test(configer)
+    
+    ## 保存数据
+    avg_acc  = np.mean(data_acc,  axis=0)
+    avg_loss = np.mean(data_loss, axis=0)
+
+    table_data_acc  = np.r_[data_acc,  avg_acc.reshape(1, -1) ]
+    table_data_loss = np.r_[data_loss, avg_loss.reshape(1, -1)]
+    table_data      = np.r_[table_data_acc, table_data_loss]
+
+    np.savetxt("images/3_3_<data>_[Multi].txt", table_data)
 
 def main_3_4():
 
-    pass
+    ORDER = [i+1 for i in range(25)]  # TODO
+    usedChannels_list = [ORDER[: : i+1] for i in range(len(ORDER))]
+    splitcounts = [i for i in range(1, 6)]
+    H, W = len(splitcounts), len(usedChannels_list)
+
+    if make_table_figure:
+
+        print("Generating tables and figures [Multi]...")
+
+        table_data = np.loadtxt("images/3_4_<data>_[Multi].txt")
+        table_data_acc, table_data_loss = np.vsplit(table_data, 2)      # 按竖直方向划分为两块，即上下
+
+        ## 做表格
+        head_name = "count/波段步长"
+        rows_name = [str(i) for i in splitcounts] + ['average']
+        cols_name = [str(i+1) for i in range(W)]
+        
+        table_acc  = gen_markdown_table_2d(head_name, rows_name, cols_name, table_data_acc)
+        table_loss = gen_markdown_table_2d(head_name, rows_name, cols_name, table_data_loss)
+    
+        with open("images/3_4_<table>_[Multi].txt", 'w') as f:
+            f.write("\n\nacc\n")
+            f.write(table_acc)
+            f.write("\n\nloss\n")
+            f.write(table_loss)
+        
+        ## 作图
+        plt.figure()
+        
+        plt.subplot(121); plt.title("acc")
+        avg_acc = table_data_acc[-1]
+        plt.bar(np.arange(avg_acc.shape[0]),  avg_acc )
+        
+        plt.subplot(122); plt.title("loss")
+        avg_loss = table_data_loss[-1]
+        plt.bar(np.arange(avg_loss.shape[0]), avg_loss)
+
+        plt.savefig("images/3_4_<figure>_[Multi].png")
+
+        return
+
+    start_time = time.time(); elapsed_time = 0
+
+    data_acc  = np.zeros(shape=(H, W))
+    data_loss = np.zeros(shape=(H, W))
+    
+    for i in range(len(splitcounts)):
+        splitcount = splitcounts[i]
+        
+        for j in range(len(usedChannels_list)):
+            usedChannels = usedChannels_list[j]
+            
+            configer = get_configer(splitcount=splitcount, usedChannels=usedChannels)
+
+            elapsed_time += time.time() - start_time
+            start_time    = time.time()
+            print("Main 3.4 [Multi] [{}] {}... Elaped >>> {} min".\
+                        format(configer.splitmode, usedChannels, elapsed_time/60))
+
+            logpath = os.path.join(configer.logspath, configer.modelname)
+            if os.path.exists(logpath):
+                with open(os.path.join(logpath, 'test_log.txt'), 'r') as f:
+                    test_log = f.readlines()
+                    data_acc[i, j], data_loss[i, j] = parse_log(test_log)
+                    print(test_log)
+            else:
+                train(configer)
+                data_acc[i, j], data_loss[i, j] = test(configer)
+    
+    ## 保存数据
+    avg_acc  = np.mean(data_acc,  axis=0)
+    avg_loss = np.mean(data_loss, axis=0)
+
+    table_data_acc  = np.r_[data_acc,  avg_acc.reshape(1, -1) ]
+    table_data_loss = np.r_[data_loss, avg_loss.reshape(1, -1)]
+    table_data      = np.r_[table_data_acc, table_data_loss]
+
+    np.savetxt("images/3_4_<data>_[Multi].txt", table_data)
 
 def main_3_5():
 
@@ -291,4 +447,6 @@ if __name__ == "__main__":
     
     # main_3_1()
 
-    main_3_2()
+    # main_3_2()
+
+    main_3_3()
